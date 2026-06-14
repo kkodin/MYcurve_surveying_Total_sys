00d797d61f4f4a2fda53e24bfc605da5e7bb8dce
kkodin <kkodin@gmail.com>
2026-06-14 21:35:12 +0900

    service-worker: キャッシュバージョンをm-app-002-v1に更新、不要ファイルをリストから削除

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
-﻿const CACHE_NAME = "m-app-001-v18";
+const CACHE_NAME = "m-app-002-v1";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon.svg",
-  "./README_M_APP_001.txt",
+  "./README.txt",
   "./00_basic_input.html",
   "./01_alignment_calculation.html",
   "./02_alignment_visual.html",
   "./03_joint_opening_check.html",
   "./04_jacking_force.html",
-  "./05_measurement_horizontal_error.html",
-  "./鬢ｨ逕ｺ荳区ｰｴ_・掻2_19譛ｬ逶ｮ貂ｬ螳夂ｵ先棡.json",
-  "./鬢ｨ逕ｺ荳区ｰｴ_・掻2_蝓ｺ邱壹ョ繝ｼ繧ｿ.json"
+  "./05_measurement_horizontal_error.html"
 ];
 
 self.addEventListener("install", (event) => {
@@ -44,5 +42,3 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
-
